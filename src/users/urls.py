166e90606ce7fd5e0b169725708b8be0bfcc3010
Zontax <jrvadim04@gmail.com>
2024-10-30 16:58:28 +0200

    Remove register (user registration only by admin)

--- src/users/urls.py
+++ src/users/urls.py
@@ -4,7 +4,6 @@ from users import views
 app_name = 'users'
 
 urlpatterns = [
-    path('register/', views.UserRegisterView.as_view(), name='register'),
     path('login/', views.UserLoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('register-wait/', views.ResetWaitView.as_view(), 
