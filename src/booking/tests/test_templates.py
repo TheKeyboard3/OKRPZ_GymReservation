e958ed92960fc3e7911cf92ebcb207c15c76a721
Zontax <jrvadim04@gmail.com>
2024-11-26 21:12:49 +0200

    Add test_templates for booking (access to pages and data in templates)

--- src/booking/tests/test_templates.py
+++ src/booking/tests/test_templates.py
@@ -1,24 +1,61 @@
 from django.test import TestCase
-from users.models import TrainerProfile, User
+from users.models import User, ClientProfile, TrainerProfile
 
 
 class BookingTemplateTest(TestCase):
 
     def setUp(self):
-        self.user = User.objects.create_user(
-            username='max',
+        self.user1 = User.objects.create_user(
+            username='1',
+            first_name='Адміністратор',
+            last_name='Вадим',
+            email='admin1@gmail.com',
+            password='123456',
+            is_active=True,
+            is_staff=True)
+        
+        self.tr1 = User.objects.create_user(
+            username='2',
             first_name='Максим',
             last_name='Березюк',
             email='berez@gmail.com',
-            password='password')
-        self.trainer = TrainerProfile.objects.create(
-            user=self.user,
-            bio='Опис тренера та його види зайнятості',
-            phone_number='+380680382525'
-        )
+            password='123456',
+            is_active=True)
 
-    def test_booking_detail_template_correct(self):
-        response = self.client.get('/trainers/1/')
+        TrainerProfile.objects.create(
+            user=self.tr1,
+            avatar=None,
+            bio='Опис тренера та його види зайнятості',
+            phone_number='+380680382525')
+
+        self.tr2 = User.objects.create_user(
+            username='3',
+            first_name='Назар',
+            last_name='Боднар',
+            email='bodnar@gmail.com',
+            password='123456',
+            is_active=True)
+
+        TrainerProfile.objects.create(
+            user=self.tr2,
+            avatar=None,
+            bio='Опис',
+            phone_number='+380680382525')
+
+        self.client1 = User.objects.create_user(
+            username='4',
+            first_name='Віктор',
+            last_name='Вуйко',
+            email='vuico@gmail.com',
+            password='123456',
+            is_active=True)
+
+        ClientProfile.objects.create(
+            user=self.client1,
+            avatar=None)
+
+    def test_booking_detail_without_login(self):
+        response = self.client.get(f'/trainers/{self.tr1.pk}/')
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'base.html')
@@ -28,8 +65,31 @@ class BookingTemplateTest(TestCase):
                             '<title>Максим Березюк - Fitness First</title>', html=True)
         self.assertContains(response, 'Опис тренера та його види зайнятості')
 
-    def test_trainers_page_template_correct(self):
-        response = self.client.get('/trainerss')
+    def test_trainers_list_without_login(self):
+        response = self.client.get('/trainers/')
 
-        self.assertEqual(response.status_code, 404, 
-                         'Перевірка доступу неавторизованим користувачем')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Максим Березюк')
+        self.assertContains(response, 'Назар Боднар')
+
+    def test_create_work_schedule_if_admin(self):
+        login = self.client.login(email='admin1@gmail.com', password='123456')
+        self.assertTrue(login)
+        response = self.client.get(f'/trainers/{self.tr1.pk}/schedule/add/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Максим Березюк')
+
+    def test_create_work_schedule_if_trainer(self):
+        login = self.client.login(email='berez@gmail.com', password='123456')
+        self.assertTrue(login)
+        response = self.client.get(f'/trainers/{self.tr1.pk}/schedule/add/')
+
+        self.assertEqual(response.status_code, 403)
+
+    def test_create_work_schedule_if_client(self):
+        login = self.client.login(email='vuico@gmail.com', password='123456')
+        self.assertTrue(login)
+        response = self.client.get(f'/trainers/{self.tr1.pk}/schedule/add/')
+
+        self.assertEqual(response.status_code, 403)
